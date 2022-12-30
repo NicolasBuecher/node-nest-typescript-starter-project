c0d1d33db8490593293de1d54b57b42852269082
nicolasb <buecher.nicolas@gmail.com>
2022-12-30 20:16:37 +0100

    feat: create nest api folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world.");
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const DEFAULT_PORT = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || DEFAULT_PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap();
